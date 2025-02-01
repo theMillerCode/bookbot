aa4b93cad2e6f3b7a15ca5b5e6bfa0d93b3cb60f
theMillerCode <jsnmllr30@gmail.com>
2025-02-01 14:06:17 -0600

    Completed bookbot project

--- main.py
+++ main.py
@@ -1,23 +1,35 @@
+path = "books/frankenstein.txt"
+
 def main():
-    with open("books/frankenstein.txt") as f:
+    with open(path) as f:
+        #variables
         file_contents = f.read()
         char_set = set(file_contents.lower())
-        char_dict = {}
         new_dict = {}
         count = 0
 
-        #print(file_contents)
+        #functions
+        def sort_on(dict):
+            return dict["num"]
 
-        print("Word count:",len(file_contents.split()))
+        print(f"--- Begin report of {path} ---")
 
-#print(file_contents.lower().count(" "))
+        print(len(file_contents.split()), "words found in the document")
 
         for char in char_set:
             new_dict.update({char : file_contents.lower().count(char) })
+
+        list_of_dicts = [{'letter': key, 'num': value} for key, value in new_dict.items()]
+
+        list_of_dicts.sort(reverse=True, key=sort_on)
+
+        for dicts in list_of_dicts:
+            items = list(dicts.items())
+            first_key, first_value = items[0]
+            second_key, second_value = items[1]
+
+            if first_value.isalpha():
+                print(f"The '{first_value}' character was found {second_value} times")
+
             
-        print(new_dict)
-
-
-    
-
 main()
