9ae98bc788208570f93b2bc9d658cd598c8ab98f
theMillerCode <jsnmllr30@gmail.com>
2025-02-23 14:34:44 -0600

    updated main to read in book and return word count

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
-print("greetings boots")
+#get the book to analyse
+def get_book_text():
+    #filepath
+    path_to_file = "books/frankenstein.txt"
+
+    #open file
+    with open(path_to_file) as f:
+        file_contents = f.read()
+    return file_contents
+
+#count the # of words in book
+def num_of_words():
+    word_count = get_book_text().split()
+    return f"{len(word_count)} words found in the document"
+    
+def main():
+    print(num_of_words())
+
+main()
